5f831d9f0ca2fb28c31f8e465042ea1b4fc7911f
heodongun <heodongun0922@gmail.com>
2025-10-15 17:49:09 +0900

    init

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
-# import boto3
+
 from flask import Flask, request, Response
 
 app = Flask(__name__)
-# s3 = boto3.client('s3',
-#                   aws_access_key_id=con.AWS_ACCESS_KEY,
-#                   aws_secret_access_key=con.AWS_SECRET_KEY)
+
 
 @app.route('/')
 def index():
@@ -14,14 +12,6 @@ def index():
 def health():
     return Response("Success Health Check", status=200)
 
-# @app.route('/upload', methods=['POST'])
-# def upload_image():
-#     file = request.files['image']
-#     if file:
-       # upload_fileobj(파일자체, 버킷이름, s3키값)
-       #  s3.upload_fileobj(file, con.BUCKET_NAME, 'image/{}'.format(file.filename))
-       #  return Response("upload success", status=200)
-    # return Response("No file", status=404)
 
 if __name__ == '__main__':
     # db = pymysql.connect(host=con.DB_HOST, user=con.DB_USER, password=con.DB_PASSWORD, db=con.DB_SCHEMA)
